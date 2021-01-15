1912d83f85481ec669de7f384f91096fb6e1bf7a
Sourabh Bagrecha <sourabhbagrecha@gmail.com>
2021-01-15 21:45:50 +0530

    optimized kadane's algorithm

--- kadanes-algorithm.js
+++ kadanes-algorithm.js
@@ -4,16 +4,10 @@ const arr = [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]
 const findSubsetWithMaxSum = (arr) => {
   let maxSum = 0
   let runningSum = 0
-  let minSum = 0
   for (let i = 0; i < arr.length; i++) {
-    if (runningSum + arr[i] < minSum) {
-      runningSum = 0
-    } else {
-      runningSum += arr[i]
-      if (runningSum > maxSum) {
-        maxSum = runningSum
-      }
-    }
+    runningSum += arr[i]
+    if (runningSum<0) runningSum = 0
+    maxSum = Math.max(runningSum, maxSum)
   }
   return maxSum
 }
